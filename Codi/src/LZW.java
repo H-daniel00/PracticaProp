613f2a84ef7f357070d647ce18db1d2f0d48ecea
xaviv <xavier.valls.ruiz@est.fib.upc.edu>
2019-12-20 18:45:43 +0100

    d

--- Codi/src/LZW.java
+++ Codi/src/LZW.java
@@ -20,9 +20,9 @@ public class LZW extends Algoritme{
         if(funcio == 0) this.input = input;
         else this.in =  new DataInputStream( new BufferedInputStream(input));
         this.out = new DataOutputStream(output);
-    } //contructora descomprimir
+    }
 
-    private void llegir_input() throws UnsupportedEncodingException, IOException{
+    private void llegir_input() throws IOException{
        String cadena;
          BufferedReader i = new BufferedReader(new InputStreamReader(input, "utf-8"));
          while ((cadena = i.readLine()) != null) {
@@ -66,8 +66,6 @@ public class LZW extends Algoritme{
         }
         long time_fin = System.nanoTime();
         temps = time_fin - time_ini;
-        //midaC = out.size();
-
         return output;
 
 
@@ -82,7 +80,6 @@ public class LZW extends Algoritme{
 
 
         List<Integer> text_in = new ArrayList<Integer>();
-        //poso el text codificat a una llista d'enters
         double punter = 0;
         int afegir = 0;
         while(in.available() > 0){
@@ -109,11 +106,8 @@ public class LZW extends Algoritme{
            if(mida_diccionari < MAX_SIZE) diccionari.put(mida_diccionari++, codViejo + cadena.charAt(0));
             codViejo = cadena;
         }
-
-        System.out.println("textout:" + textOut);
         byte[] o = textOut.toString().getBytes();
         out.write(o);
-        System.out.println("mida diccionari:" + mida_diccionari);
         return output;
     }
 
